08a48627cc86cc68ad08ca2dbc7b08b744baea3b
TracerStudySmekdapapa <habibiexd@gmail.com>
2025-11-24 02:33:58 +0700

    font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -40,6 +40,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
     <link href="https://fonts.bunny.net/css?family=playfair-display:400,500,600,700,800,900" rel="stylesheet" />
+    <link href="https://fonts.bunny.net/css?family=poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
     @routes
     @viteReactRefresh
     @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
